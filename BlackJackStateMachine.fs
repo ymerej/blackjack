2af38a0541df2aa4e6a2200c64eec8ce43193ecd
Jeremy Novak <novakja@hotmail.com>
2024-08-08 07:49:34 -0500

    Initial state machine

---
+++ BlackJackStateMachine.fs
@@ -0,0 +1,48 @@
+﻿open System
+
+type blackJackStateMachine =
+    | DealPlayerFirstCard of Game
+    | DealDealerFirstCard of Game
+    | DealPlayerSecondCard of Game
+    | DealDealerSecondCard of Game
+    | PlayerTurn of Game * PlayerNumber
+    | DealerTurn of Game
+    | TallyRound of Game
+    | EndGame of Game
+and Game = {
+    Players: Player list
+    Dealer: Dealer
+}
+and Player = {
+    Number: PlayerNumber
+    Hand: Card list
+    Wins: int
+}
+and Dealer = {
+    Hand: Card list
+    Wins: int
+}
+and PlayerNumber = int
+and Card = {
+    Suit: Suit
+    Rank: Rank
+}
+and Suit =
+    | Clubs
+    | Diamonds
+    | Hearts
+    | Spades
+and Rank =
+    | Two
+    | Three
+    | Four
+    | Five
+    | Six
+    | Seven
+    | Eight
+    | Nine
+    | Ten
+    | Jack
+    | Queen
+    | King
+    | Ace
